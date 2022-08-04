ff6e480149a25233233ace2edf0f3eb2c4971979
John Helmert III <ajak@gentoo.org>
2022-08-03 23:22:27 -0500

    mozsec: update
    Signed-off-by: John Helmert III <ajak@gentoo.org>

--- mozsec.py
+++ mozsec.py
@@ -35,34 +35,37 @@ def grok_cves(soup):
     return [header.find('a')['href'][1:]
             for header in soup.find_all('h4', {'class': 'level-heading'})]
 
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} offset")
+        sys.exit(0)
+    soup = bs(urldata(BASE + '/en-US/security/advisories/'), features='lxml')
+    links = soup.find('article').find_all('ul')[int(sys.argv[1]) + 1].find_all('a')
+    advisories = [a['href'] for a in links]
+    soups = [bs(urldata(BASE + path), features='lxml') for path in advisories]
 
-soup = bs(urldata(BASE + '/en-US/security/advisories/'), features='lxml')
-links = soup.find('article').find_all('ul')[1].find_all('a')
-advisories = [a['href'] for a in links]
-soups = [bs(urldata(BASE + path), features='lxml') for path in advisories]
+    ff_ver = []
+    ff_cves = []
+    tb_ver = []
+    tb_cves = []
 
-ff_ver = []
-ff_cves = []
-tb_ver = []
-tb_cves = []
+    for soup in soups:
+        if is_firefox(soup):
+            ff_ver += [get_versions(soup)]
+            ff_cves += grok_cves(soup)
+        elif is_tb(soup):
+            tb_ver += [get_versions(soup)]
+            tb_cves += grok_cves(soup)
 
-for soup in soups:
-    if is_firefox(soup):
-        ff_ver += [get_versions(soup)]
-        ff_cves += grok_cves(soup)
-    elif is_tb(soup):
-        tb_ver += [get_versions(soup)]
-        tb_cves += grok_cves(soup)
+    tracker_cves = set(ff_cves).intersection(set(tb_cves))
 
-tracker_cves = set(ff_cves).intersection(set(tb_cves))
+    if tracker_cves:
+        print('Tracker CVEs: ' + ', '.join(tracker_cves))
 
-if tracker_cves:
-    print('Tracker CVEs: ' + ', '.join(tracker_cves))
+    if ff_ver:
+        print(', '.join(ff_ver))
+        print(', '.join(set(ff_cves).difference(tracker_cves)))
 
-if ff_ver:
-    print(', '.join(ff_ver))
-    print(', '.join(set(ff_cves)))
-
-if tb_ver:
-    print(', '.join(tb_ver))
-    print(', '.join(set(tb_cves)))
+    if tb_ver:
+        print(', '.join(tb_ver))
+        print(', '.join(set(tb_cves).difference(tracker_cves)))
